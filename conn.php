6e0bffa6edb43960d0fd8c5ce01572c90c467988
Arekhandia Harry <harry_p30@yahoo.com>
2022-08-21 22:14:21 +0100

    removed old config file

--- conn.php
+++
@@ -1,9 +0,0 @@
-<?php
-define('SERVER', 'localhost');
-define('DATABASE', 'bivr');
-define('USER', ' root');
-define('PASSWORD', '');
-
-$conn = new mysqli(SERVER, USER, PASSWORD, DATABASE);
-if ($conn->connect_error)
-    die('Failed to connect to server' . $conn->connect_error);
