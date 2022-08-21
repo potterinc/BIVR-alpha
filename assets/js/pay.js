e2284e312c225e077de5d311d4bdb66a60559ccf
Arekhandia Harry <harry_p30@yahoo.com>
2022-08-21 22:08:53 +0100

    Fixex mailer notification bug

--- assets/js/pay.js
+++ assets/js/pay.js
@@ -16,8 +16,8 @@ $('#pay').on('click', () => {
             pay: () => {
                 const paystack = new PaystackPop();
                 paystack.newTransaction({
-                    key: 'pk_test_a78dba4dcc45f5f94f55f86170449cece2bc0bd8',
-                    // key:'pk_live_7d4d8f6a7703161ed8f58f3cf0f401d4ae4d7902 ',
+                    // key: 'pk_test_a78dba4dcc45f5f94f55f86170449cece2bc0bd8',
+                    key:'pk_live_7d4d8f6a7703161ed8f58f3cf0f401d4ae4d7902 ',
                     email: Subscriber.email,
                     amount: Subscriber.price,
                     ref: 'BIVR' + Math.floor((Math.random() * 1000000000) + 1),
@@ -86,6 +86,9 @@ validateInput = (inputArgs) => {
     for (let formInput = 0; formInput < validInput.length; formInput++) {
         if (validInput.get(formInput).value == null || validInput.get(formInput).value == '') {
             validInput[formInput].placeholder = 'This field is required';
+            validInput[formInput].style.borderWidth = '5px';
+            validInput[formInput].style.borderStyle = 'solid';
+            validInput[formInput].style.borderLeftColor = '#a00';
             return false;
         }
     }
